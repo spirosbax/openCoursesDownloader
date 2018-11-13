b498c35359f2e89165fb675a26591d4153413804
spirosbax <sprbax@gmail.com>
2018-11-14 01:00:06 +0200

    fix problems with some pdf files

--- open.py
+++ open.py
@@ -1,3 +1,7 @@
+# bad thing with python is:
+# you have dependancies...
+
+import requests
 from requests import get
 from requests.exceptions import RequestException
 from bs4 import BeautifulSoup
@@ -5,16 +9,39 @@ from contextlib import closing
 import wget
 
 def main():
+    # get user course number
     course_number = input("Δώσε κωδικό μαθήματος(πχ. DDI123): ")
+    # create link string
     link = "https://opencourses.ionio.gr/modules/document/?course=" + course_number
 
-    soup = BeautifulSoup(simple_get(link), 'html.parser')
-    # soup = BeautifulSoup(open('./index.html').read(), 'html.parser')
-    rows = soup.findAll('a', {'class': "fileURL fileModal"})
-    downloads = [row.attrs["href"] for row in rows]
-    for file in downloads:
-        filename = wget.download(file)
+    # soup = BeautifulSoup(open('./index.html').read(), 'html.parser') # read
+    # from html file
 
+    # get html page and create soup
+    soup = BeautifulSoup(simple_get(link), 'html.parser')
+    # print(soup)
+
+    # get all <a> elements
+    rows = soup.findAll('a', {'class': "fileURL fileModal"})
+
+    # for every row, extract href attribute and append it to a list
+    # print(rows)
+    downloads = [row.attrs["href"] for row in rows]
+    for file in downloads: # for every file
+        print(file)
+        filename = download_file(file) # download and store file
+
+
+def download_file(url):
+    local_filename = url.split('/')[-1]
+    # NOTE the stream=True parameter
+    r = requests.get(url, stream=True)
+    with open(local_filename, 'wb') as f:
+        for chunk in r.iter_content(chunk_size=1024):
+            if chunk: # filter out keep-alive new chunks
+                f.write(chunk)
+                #f.flush() commented by recommendation from J.F.Sebastian
+    return local_filename
 
 def simple_get(url):
     """
